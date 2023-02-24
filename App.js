1c5a9d2eba4bd20f7eda9acba404c7612f155b34
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-02-24 10:24:05 -0500

    react native app created, new reminder button created

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+// type AppProps = {
+//   name: string;
+// };
+
+// const App = (props: AppProps) => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>hewwo {props.name}</Text>
+//       {/* <StatusBar style="auto" /> */}
+//     </View>
+//   );
+// }
+
+// const LotsofNames = () => {
+//   return(
+//     <View style={styles.container}>
+//       <App name="Sally" />
+//       <App name="Bailey" />
+//     </View>
+//   )
+// }
+
+const App = () => {
+  const [create, setCreate] = useState(false)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+      {/* //form to show new reminder */}
+      {create ? 
+        <Text> create is true</Text>
+      :
+      <Button 
+        title="Create new reminder?"
+        onPress={()=> setCreate(true)}
+      />
+      }
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +50,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// export default LotsofNames;
+export default App;
