2d87f640f686380a3d4fecea9443aa1de544e17a
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-02-24 21:46:15 -0500

    got rid of empty first reminder

--- App.js
+++ App.js
@@ -8,8 +8,8 @@ import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 const App = () => {
   const [remind, onChangeRemind] = React.useState()
-  const [reminders, setReminders] = useState([{}])
-  const [count, setCount] = useState(1)
+  const [reminders, setReminders] = useState([])
+  const [count, setCount] = useState(0)
     //date time picker
   const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState('date');
@@ -72,22 +72,21 @@ const App = () => {
           }}
         />
       <Text>Reminders</Text>
-      {/* <View style={styles.reminders}></View> */}
         {reminders.map((reminder)=> {
           return(
+            <>
             <View
             style={styles.reminders}
             key={reminder.id}
-
             >
               <BouncyCheckbox 
                 size={25}
-                
                 onPress={(isChecked: boolean) => {}} 
                 text = {reminder.name} 
               />
               <Text>at {reminder.time}</Text>
             </View>
+            </>
           )
         })
 
@@ -98,129 +97,6 @@ const App = () => {
 }
 
 
-
-const NewForm = (props) => {
-  //react hook form
-   const { register, setValue, handleSubmit, control, reset, formState: { errors } } = useForm(defaultValues);
-  const onSubmit = data => {
-    console.log(data);
-    props.setReminders(data)
-  };
-
-  // //date time picker
-  // const [date, setDate] = useState(new Date());
-  // const [mode, setMode] = useState('date');
-  // const [show, setShow] = useState(false);
-
-  // const onChange = (event, selectedDate) => {
-  //   const currentDate = selectedDate;
-  //   // setShow(false);
-  //   setDate(currentDate);
-  // };
-
-  // const showMode = (currentMode) => {
-  //   if (Platform.OS === 'android') {
-  //     setShow(false);
-  //     // for iOS, add a button that closes the picker
-  //   }
-  //   setShow(true);
-  //   setMode(currentMode);
-  // };
-
-  // const showDatepicker = () => {
-  //   showMode('date');
-  // };
-
-  // const showTimepicker = () => {
-  //   showMode('time');
-  // };
-   
-  // const onError: SubmitErrorHandler<FormValues> = (errors, e) => {
-  //   return console.log(errors)
-  // }
-
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Reminder</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="reminder"
-        rules={{ required: true }}
-      />
-
-
-{/* <View> */}
-      <Text style={styles.label}>Select Date & Time</Text>
-      
-      <Button onPress={showDatepicker} title="Show date picker!" />
-      <Button onPress={showTimepicker} title="Show time picker!" />
-      {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          onChange={onChange}
-        />
-        )}     
-      <Text>selected: {date.toLocaleString()}</Text>
-
-    {/* </View> */}
-
-      {/* <Text style={styles.label}>Date & Time</Text>
-      <Controller
-        control={control}
-        render={({field: { onChange, onBlur, value }}) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-        name="date"
-        rules={{ required: true }}
-      /> */}
-
-      {/* <View style={styles.button}>
-        <Button
-          style={styles.buttonInner}
-          color
-          title="Reset"
-          onPress={() => {
-            reset({
-              reminder: 'reminder to pick up laundry detergent',
-              // date: 'date and time'
-            })
-          }}
-        />
-      </View> */}
-      
-     
-
-      <View style={styles.button}>
-        <Button
-          style={styles.buttonInner}
-          color
-          title="Submit"
-          onPress={handleSubmit(onSubmit)}
-        />
-      </View>
-
-    </View>
-
-  );
-  
-}
 
 const styles = StyleSheet.create({
   container: {
